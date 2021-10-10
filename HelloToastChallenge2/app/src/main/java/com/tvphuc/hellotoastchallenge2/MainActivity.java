ea261ebc01c6b7448530a2ea80cd87803008a7c8
phuctv302 <phuc.tv194354@sis.hust.edu.vn>
2021-10-10 21:11:41 +0700

    01.2 Part A + B include code challenge

---
+++ HelloToastChallenge2/app/src/main/java/com/tvphuc/hellotoastchallenge2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.tvphuc.hellotoastchallenge2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowCount = findViewById(R.id.show_count);
+
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
